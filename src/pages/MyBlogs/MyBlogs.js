2957b04f99a97e4a4dec8ece4bae2b643786d7b7
maheshtatipamula <mahesh.tatipamula43@gmail.com>
2023-08-18 20:33:14 +0530

    third commit

--- src/pages/MyBlogs/MyBlogs.js
+++ src/pages/MyBlogs/MyBlogs.js
@@ -32,7 +32,10 @@ const MyBlogs = () => {
             posts.map((post) => (
               <div className="post" key={post.id}>
                 <div className="img">
-                  <img src={`../../../uploads/${post?.img}`} alt="post" />
+                  <img
+                    src={`https://blog-node-mysql.onrender.com/${post?.img}`}
+                    alt="post"
+                  />
                 </div>
                 <div className="content">
                   <Link className="link" to={`/post/${post.id}`}>
